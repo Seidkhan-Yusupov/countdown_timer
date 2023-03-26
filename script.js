70be74b3a2b122f456a6b7188498723130ba5cf8
Seidkhan Yusupov <saidxonyusupov@gmail.com>
2023-03-26 17:00:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+const comingYear = new Date().getFullYear();
+const newYears = "2 May " + comingYear;
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    // there are 3600 seconds in an hour
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor((totalSeconds / 60) % 24);
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days, hours, mins, seconds);
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+// now if there is less than 10 remaining zero shows up in the front
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+countdown();
+
+setInterval(countdown, 1000);
